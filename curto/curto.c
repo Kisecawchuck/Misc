c47494bf1c775d62e239176c9b8d9719decb9c8a
Igor <iny5051@gmail.com>
2025-05-10 00:17:13 -0300

    Typedef nos Enums

--- curto/curto.c
+++ curto/curto.c
@@ -19,9 +19,8 @@ const char* nome_jogador_17076671() {
 // Você recebe seu id (posição na mesa) e o número total de jogadores.
 // Guarde essas informações se quiser usá-las em sua estratégia.
 void iniciar_17076671(int meu_id, int total_jogadores) {
-	// Exemplo:
-	// id = meu_id;
-	// num_jogadores = total_jogadores;
+    id = meu_id;
+    num_jogadores = total_jogadores;
 }
 
 // Esta função é chamada no início de cada rodada.
@@ -36,6 +35,9 @@ void nova_rodada_17076671(int rodada, Carta carta_virada, Valor manilha, int n_c
 	// for (int i = 0; i < n_cartas; i++) {
 	// mao[i] = minha_mao[i];
 	// }
+    for (int i = 0; i < n_cartas; i++) {
+        mao[i] = minha_mao[i];
+    }
 }
 
 // Esta função define quantas mãos você pretende vencer na rodada.
@@ -44,7 +46,7 @@ void nova_rodada_17076671(int rodada, Carta carta_virada, Valor manilha, int n_c
 int apostar_17076671() {
 	// Exemplo simples: apostar sempre metade das cartas (arredondado para baixo)
 	// return num_cartas / 2;
-	return 0; // Substitua pelo valor da sua aposta
+	return num_cartas / 2; // Substitua pelo valor da sua aposta
 }
 
 // Esta função escolhe qual carta jogar.
@@ -53,12 +55,14 @@ int apostar_17076671() {
 // Use uma estratégia para marcar cartas já usadas, por exemplo:
 // - marcar com `mao[i].valor = -1`
 // - procurar a próxima carta com `mao[i].valor >= 0`
-6
-
 int jogar_17076671(Carta* mesa, int num_na_mesa) {
 	// Exemplo mínimo: sempre retorna a primeira carta válida
 	// for (int i = 0; i < num_cartas; i++) {
 	// if (mao[i].valor >= 0) return i;
 	// }
 	return 0; // Substitua pela sua lógica de jogo
+}
+
+int main() {
+    return 0;
 }
